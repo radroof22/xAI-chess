22660fbeb0f457ec09a4d4e11adc9b69ec0e7ff3
Rohan Mehta <rmehta98@gatech.edu>
2024-12-06 17:42:58 -0500

    fix add perturber

--- sarfa/perturbation_handler.py
+++ sarfa/perturbation_handler.py
@@ -1,9 +1,25 @@
 import chess
 from .utils import get_pos_obj, get_all_pos
 from typing import Generator
-class RemovalPerturber:
+
+class Perturber:
     def __init__(self, board: chess.Board):
         self.board = board
+
+    def perturb_position(self, position_str: str) -> chess.Board | None:
+        raise NotImplementedError("Need to implement perturb position function.")
+
+    def process(self) -> Generator[tuple[chess.Board, str], None, None]:
+        """
+        Generator that iterates over all board positions and yields perturbed instances.
+        Each yield contains the perturbed board and the position string that was perturbed.
+        """
+        for position_str in get_all_pos():
+            perturbed_board = self.perturb_position(position_str)
+            if perturbed_board:
+                yield perturbed_board, position_str
+        
+class RemovalPerturber(Perturber):
         
     def perturb_position(self, position_str: str) -> chess.Board | None:
         position = get_pos_obj(position_str)
@@ -18,19 +34,8 @@ class RemovalPerturber:
         
         return perturbed_board
         
-    def process(self) -> Generator[tuple[chess.Board, str], None, None]:
-        """
-        Generator that iterates over all board positions and yields perturbed instances.
-        Each yield contains the perturbed board and the position string that was perturbed.
-        """
-        for position_str in get_all_pos():
-            perturbed_board = self.perturb_position(position_str)
-            if perturbed_board:
-                yield perturbed_board, position_str
 
-class AddPerturber:
-    def __init__(self, board: chess.Board):
-        self.board = board
+class AddPerturber(Perturber):
         
     def perturb_position(self, position_str: str) -> chess.Board | None:
         position = get_pos_obj(position_str)
@@ -42,19 +47,7 @@ class AddPerturber:
             # add pawn same color as current move
             new_piece = chess.Piece(chess.PAWN, chess.WHITE) if self.board.turn else chess.Piece(chess.PAWN, chess.BLACK)
             perturbed_board.set_piece_at(position, new_piece)
-            if perturbed_board.was_into_check():
-                return
 
             return perturbed_board
         else:
             return
-        
-    def process(self) -> Generator[tuple[chess.Board, str], None, None]:
-        """
-        Generator that iterates over all board positions and yields perturbed instances.
-        Each yield contains the perturbed board and the position string that was perturbed.
-        """
-        for position_str in get_all_pos():
-            perturbed_board = self.perturb_position(position_str)
-            if perturbed_board:
-                yield perturbed_board, position_str
